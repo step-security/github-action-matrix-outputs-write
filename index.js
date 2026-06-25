63b5a2c2a0ff1db4a4338f2acd46225be401f968
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-25 12:17:08 +0530

    feat: onboarding github-action-matrix-outputs-write

---
+++ index.js
@@ -0,0 +1,92 @@
+const core = require('@actions/core');
+const yaml = require('yaml')
+const {DefaultArtifactClient} = require('@actions/artifact')
+const crypto = require('crypto');
+const fs = require('fs');
+
+try {
+    const step_name = core.getInput('matrix-step-name');
+    const matrix_key = core.getInput('matrix-key');
+    const outputs = core.getInput('outputs');
+
+    core.debug("step_name:")
+    core.debug(step_name)
+
+    core.debug("matrix_key:")
+    core.debug(matrix_key)
+
+    core.debug("outputs:")
+    core.debug(outputs)
+
+    function isEmptyInput(value) {
+        return value === null || value.trim() === "";
+    }
+
+    if (isEmptyInput(step_name) && !isEmptyInput(matrix_key)) {
+        core.setFailed("`matrix-step-name` can not be empty when `matrix-key` specified");
+        return
+    }
+
+    if (!isEmptyInput(step_name) && isEmptyInput(matrix_key)) {
+        core.setFailed("`matrix-key` can not be empty when `matrix-step-name` specified");
+        return
+    }
+
+    const matrix_mode = !isEmptyInput(step_name) && !isEmptyInput(matrix_key)
+
+    if (!isEmptyInput(outputs)) {
+        try {
+            yaml.parse(outputs)
+        }
+        catch (error) {
+            message = `Outputs should be valid YAML 
+---------------------
+${outputs}
+---------------------
+${error}`;
+            core.setFailed(message);
+            return
+        }
+    }
+
+    const outputs_struct = !isEmptyInput(outputs) ? yaml.parse(outputs) : {}
+
+    Object.keys(outputs_struct).forEach(function(key, index) {
+        core.setOutput(key, outputs_struct[key])
+    });
+
+    core.debug("outputs_struct:")
+    core.debug(outputs_struct)
+
+    core.debug("JSON.stringify(outputs_struct):")
+    core.debug(JSON.stringify(outputs_struct))
+
+    core.setOutput('result', JSON.stringify(outputs_struct))
+
+    if (!isEmptyInput(outputs) && matrix_mode) {
+        const artifact_content = { [matrix_key]: outputs_struct }
+        const filename = "./" + step_name +"-"+ matrix_key
+
+        fs.writeFileSync(filename, JSON.stringify(artifact_content));
+        const fileBuffer = fs.readFileSync(filename);
+        const hashSum = crypto.createHash('sha256');
+        hashSum.update(fileBuffer);
+
+        const hex = hashSum.digest('hex');
+
+        const artifactClient = new DefaultArtifactClient();
+        const artifactName = step_name +"-"+ matrix_key +"-"+ hex;
+        const files = [
+            filename,
+        ]
+
+        const rootDirectory = '.' // Also possible to use __dirname
+        const options = {
+            continueOnError: false
+        }
+
+        artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
